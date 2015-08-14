e8c7e322c8f56725d925409b45b35b9ab16ce33e
Thembelani Momoti <thembelani@digital.pepkorit.com>
2015-08-14 16:03:16 +0200

    14-08-2015 Added Home page and contact page. Home page needs work

--- public/app.js
+++ public/app.js
@@ -1,18 +1,53 @@
-var myApp = angular.module('myApp', [] );
+var myApp = angular.module('myApp', ['ui.router'] );
 
 myApp.controller('mainController', function($scope){
-	console.log("before Image");
+	
 	$scope.backGroundImage = 'Wood-Background-Pictures-Free-Download.jpg';
-	//$scope.backGroundImage = '';
-	console.log("after Image"); 
 });
 
-//myApp.config(function($stateProvider, $urlRouterProvider){
-	
-	//$urlRouterProvider.otherwise('/home');
+myApp.controller('recentJobsController', function($http, $scope){
+	$http.get('https://jobs.github.com/positions.json?description=python&location=new+york')
+		.success(function(data){
+			$scope.jobs = data;
+		})
+});
 
-	//$stateProvider
-	//	.state('header',{
-	///		templateUrl : 'header.html'
-	//	});
-//});
+myApp.controller('carouselController', function($scope){
+	$scope.interval = 5000;
+	$scope.noWrapSlides = false;
+
+	var slides = $scope.slides=[];
+
+	$scope.addSlide = function(){
+		var newWidth =600 + slides.length+1;
+		slides.push({
+
+		});
+	};
+
+	for (var i=0; i<2; i++){
+		$scope.addSlide();
+	}
+});
+
+myApp.config(function($stateProvider, $urlRouterProvider){
+	
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+		.state('contact',{
+			url:'/contact',
+			templateUrl: 'contact-us.html'
+
+		})
+		.state('home',{
+			url : '/home',
+			views : {
+				''				 : {templateUrl: 'home-partial.html'},
+				'homeMain@home'  : {templateUrl: 'home-left-partial.html'},
+				'recentJobs@home': {templateUrl: 'home-right-partial.html',
+									controller : 'recentJobsController'}
+
+			} 
+		});
+});
